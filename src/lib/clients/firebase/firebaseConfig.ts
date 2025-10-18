81c6c7f5354565aff8678ddfceb5180606b6ebb7
Carolina Lunas <carolinalunasfarah@gmail.com>
2025-10-18 09:33:18 -0300

    install firebase, add config, modify env

---
+++ src/lib/clients/firebase/firebaseConfig.ts
@@ -0,0 +1,18 @@
+import ENVIRONMENT from '@/lib/environment';
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: ENVIRONMENT.FIREBASE_API_KEY,
+  authDomain: ENVIRONMENT.FIREBASE_AUTH_DOMAIN,
+  projectId: ENVIRONMENT.FIREBASE_PROJECT_ID,
+  storageBucket: ENVIRONMENT.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: ENVIRONMENT.FIREBASE_MESSAGING_SENDER_ID,
+  appId: ENVIRONMENT.FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
+
+export default app;
