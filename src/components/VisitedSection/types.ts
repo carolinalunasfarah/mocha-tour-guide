cfdf53f71ab286d5e7e3533a5fb2d5679dda7428
Carolina Lunas <carolinalunasfarah@gmail.com>
2025-12-09 20:02:17 -0300

    modify components to allow independent visited adding in admin

--- src/components/VisitedSection/types.ts
+++ src/components/VisitedSection/types.ts
@@ -1,8 +1,12 @@
+import { UseFormRegister, FieldErrors } from "react-hook-form";
 import { FormData } from "@/components/DataForm/types";
 
 type VisitedSectionProps = {
   rating: number;
   nameLowercase: string;
+  isMochaActive: boolean;
+  register: UseFormRegister<FormData>;
+  errors: FieldErrors<FormData>;
   onSubmitReady: (handleSubmit: (data: FormData) => Promise<void>) => void;
 };
 
